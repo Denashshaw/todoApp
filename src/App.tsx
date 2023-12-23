0d3f369ab7b636fc0bb8134ee5482df66d485b77
denash shaw <denashshaw@gmail.com>
2023-12-23 16:57:42 +0530

    Added tailwindcss to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <div>Denash Shaw</div>
-    </>
-  )
+  return <div className="w-full h-screen bg-black rounded-xl shadow-2xl">
+    <h1 className="text-white flex justify-center align-center">Denash Shaw</h1>
+  </div>
 }
 
-export default App
+export default App;
