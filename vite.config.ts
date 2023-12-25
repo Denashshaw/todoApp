adb1bb7b7e76d4c1022783b957c9501ceffa862f
denash shaw <denashshaw@gmail.com>
2023-12-25 18:20:54 +0530

    Deploying through github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/todoApp/",
   server: {
     port: 3000,
   },
